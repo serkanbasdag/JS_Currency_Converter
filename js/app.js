f7bcaa025c90c443c5595348d7fd92dff0d79adb
Serkan Başdağ <40989158+serkanbasdag@users.noreply.github.com>
2019-03-20 16:22:10 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,48 +15,49 @@ function change(){
               var kurValue1 = document.getElementById("kur1").options[kurIndex1].text;
               var kurIndex2 = document.getElementById("kur2").selectedIndex;
               var kurValue2 = document.getElementById("kur2").options[kurIndex2].text;
-              console.log(response);
+
               if(kurValue1 == kurValue2){
                 alert("Please choose two different currency type!");
               }
-              else if(kurValue1 == "Euro" && kurValue2 == "Dolar"){
+              else if(kurValue1 == "EUR" && kurValue2 == "USD"){
                const rate = response.rates.USD
                const doviz1 = Number(document.getElementById("doviz1").value);
                document.getElementById("doviz2").value = rate * doviz1;
+               
               }
-              else if(kurValue1 == "Euro" && kurValue2 == "İsviçre Frankı"){
+              else if(kurValue1 == "EUR" && kurValue2 == "İsviçre Frankı"){
                const rate = response.rates.CHF
                const doviz1 = Number(document.getElementById("doviz1").value);
                document.getElementById("doviz2").value = rate * doviz1;
               }
-              else if(kurValue1 == "Euro" && kurValue2 == "TRY"){
+              else if(kurValue1 == "EUR" && kurValue2 == "TRY"){
                const rate = response.rates.TRY
                const doviz1 = Number(document.getElementById("doviz1").value);
                document.getElementById("doviz2").value = rate * doviz1;
                }
 //
-               else if(kurValue1 == "Dolar" && kurValue2 == "Euro"){
+               else if(kurValue1 == "USD" && kurValue2 == "EUR"){
                 const rate = response.rates.EUR / response.rates.USD
                 const doviz1 = Number(document.getElementById("doviz1").value);
                 document.getElementById("doviz2").value = rate * doviz1;
                }
-               else if(kurValue1 == "Dolar" && kurValue2 == "İsviçre Frankı"){
+               else if(kurValue1 == "USD" && kurValue2 == "İsviçre Frankı"){
                 const rate = response.rates.CHF / response.rates.USD
                 const doviz1 = Number(document.getElementById("doviz1").value);
                 document.getElementById("doviz2").value = rate * doviz1;
                }
-               else if(kurValue1 == "Dolar" && kurValue2 == "TRY"){
+               else if(kurValue1 == "USD" && kurValue2 == "TRY"){
                 const rate = response.rates.TRY / response.rates.USD
                 const doviz1 = Number(document.getElementById("doviz1").value);
                 document.getElementById("doviz2").value = rate * doviz1;
                 }
 //
-                else if(kurValue1 == "İsviçre Frankı" && kurValue2 == "Euro"){
+                else if(kurValue1 == "İsviçre Frankı" && kurValue2 == "EUR"){
                   const rate = response.rates.EUR / response.rates.CHF
                   const doviz1 = Number(document.getElementById("doviz1").value);
                   document.getElementById("doviz2").value = rate * doviz1;
                  }
-                 else if(kurValue1 == "İsviçre Frankı" && kurValue2 == "Dolar"){
+                 else if(kurValue1 == "İsviçre Frankı" && kurValue2 == "USD"){
                   const rate = response.rates.USD / response.rates.CHF
                   const doviz1 = Number(document.getElementById("doviz1").value);
                   document.getElementById("doviz2").value = rate * doviz1;
@@ -67,12 +68,12 @@ function change(){
                   document.getElementById("doviz2").value = rate * doviz1;
                   }
 //
-                  else if(kurValue1 == "TRY" && kurValue2 == "Euro"){
+                  else if(kurValue1 == "TRY" && kurValue2 == "EUR"){
                     const rate = response.rates.EUR / response.rates.TRY
                     const doviz1 = Number(document.getElementById("doviz1").value);
                     document.getElementById("doviz2").value = rate * doviz1;
                    }
-                   else if(kurValue1 == "TRY" && kurValue2 == "Dolar"){
+                   else if(kurValue1 == "TRY" && kurValue2 == "USD"){
                     const rate = response.rates.USD / response.rates.TRY
                     const doviz1 = Number(document.getElementById("doviz1").value);
                     document.getElementById("doviz2").value = rate * doviz1;
